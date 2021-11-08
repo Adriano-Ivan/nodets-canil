2ac7ecbb2dfd9efc2c5fd03e8988afda0238fd31
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2021-11-08 18:51:24 -0300

    Organizando views de header e footer.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,16 +5,20 @@ import * as SearchController from "../controllers/searchController";
 const router = Router();
 
 router.get("/", (req, res) => {
-  PageController.home;
+  res.send("home");
+  //PageController.home;
 });
 router.get("/dogs", (req, res) => {
-  PageController.dogs;
+  res.send("dogs");
+  //PageController.dogs;
 });
 router.get("/cats", (req, res) => {
-  PageController.cats;
+  res.send("catas");
+  //PageController.cats;
 });
 router.get("/fishes", (req, res) => {
-  PageController.fishes;
+  res.send("fishes");
+  //PageController.fishes;
 });
 router.get("/search", SearchController.search);
 export default router;
